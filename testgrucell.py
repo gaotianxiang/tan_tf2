a9dee5b767175704f555cf1a9323edd62effd174
Tianxiang Gao <gtx9726@gmail.com>
2020-04-29 10:24:09 -0400

    format testgrucell.py

--- testgrucell.py
+++ testgrucell.py
@@ -2,13 +2,13 @@ from module import transforms as trans
 import numpy as np
 
 model = trans.Transformer([
-        trans.LeakyReLU(),
-        trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
-        trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
-        trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
-        trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
-        trans.LogRescale(), trans.RNNAffineCoupling()
-    ])
+    trans.LeakyReLU(),
+    trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
+    trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
+    trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
+    trans.LogRescale(), trans.RNNAdditiveCoupling(), trans.Reverse(), trans.LinearMap(), trans.LeakyReLU(),
+    trans.LogRescale(), trans.RNNAffineCoupling()
+])
 
 tx = np.arange(30).reshape(5, 6).astype(np.float32) - 15
 res = model(tx)
